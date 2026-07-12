24a5cb6e3a4c09e59f7aeaebcc693c1d12dfa9d3
grip <shiraishi@infogrip.net>
2026-07-12 12:19:35 +0900

    在庫管理A GitHub Pages版デモ公開

---
+++ config.js
@@ -0,0 +1,13 @@
+﻿// Version: github-pages-a-2026-06-23-02
+
+window.ZAIKO_A_CONFIG = {
+  appName: '在庫管理A QRスキャン',
+  appVersion: 'github-pages-a-2026-06-23-02',
+
+  // GASのウェブアプリURL（/execで終わるURL）を設定してください。
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycby0E-W5-yhiyKUmpf6_Pv0lwEgf4URSH0QXDVLSJNlfNtMwtfhOnxyn2UsUR2n3QZkVsA/exec',
+
+  // GAS側のAPIトークンと同じ値を設定します。不要なら空文字。
+  apiToken: '',
+};
+
