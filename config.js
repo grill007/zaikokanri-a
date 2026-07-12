971fc1b2b695c0a524f478086644864b5d637747
grip <shiraishi@infogrip.net>
2026-07-13 03:29:26 +0900

    GAS Web App URLを既存の稼働中デプロイに修正

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.ZAIKO_A_CONFIG = {
   appVersion: 'github-pages-a-2026-06-23-02',
 
   // GASのウェブアプリURL（/execで終わるURL）を設定してください。
-  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycby0E-W5-yhiyKUmpf6_Pv0lwEgf4URSH0QXDVLSJNlfNtMwtfhOnxyn2UsUR2n3QZkVsA/exec',
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbwgim9KYaLJznFvp3Cu-FQMfbwrz7eMx35Eq6ai76rKiJhbfw-3fcemyK58WUa4JQlU/exec',
 
   // GAS側のAPIトークンと同じ値を設定します。不要なら空文字。
   apiToken: '',
